bfa45781d63ea902aa4ca65bf5556938565d5958
𝘚𝘪𝘮𝘱𝘭𝘦_𝘉𝘰𝘺 <sksahnawaj89@gmail.com>
2026-08-13 21:02:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -60,8 +60,8 @@ async def send_start_panel(client, message, user_id):
     if has_key_link:
         # GET KEY ko aage karne ke liye yahan 3 spaces add kiye hain
         footer = (
-            f"\n\n{emojis.EMOJI_KEY_HEAD_LEFT1} {emojis.EMOJI_KEY_HEAD_LEFT2} <b>𝐇𝐨𝐰 𝐓𝐨 𝐆𝐞𝐭 𝐊𝐞𝐲</b> {emojis.EMOJI_KEY_HEAD_RIGHT1} {emojis.EMOJI_KEY_HEAD_RIGHT2}\n"
-            f"   {emojis.EMOJI_GET_KEY_LEFT} <a href='{str(get_key_link).strip()}'><b>𝐆𝐞𝐭 𝐊𝐞𝐲 </b></a> {emojis.EMOJI_GET_KEY_RIGHT}"
+            f"\n\n{emojis.EMOJI_KEY_HEAD_LEFT1} {emojis.EMOJI_KEY_HEAD_LEFT2}<b>𝐇𝐨𝐰 𝐓𝐨 𝐆𝐞𝐭 𝐊𝐞𝐲</b> {emojis.EMOJI_KEY_HEAD_RIGHT1} {emojis.EMOJI_KEY_HEAD_RIGHT2}\n"
+            f"   {emojis.EMOJI_GET_KEY_LEFT} <a href='{str(get_key_link).strip()}'><>/b>𝐆𝐞𝐭 𝐊𝐞𝐲 </b></a> {emojis.EMOJI_GET_KEY_RIGHT}"
         )
     else:
         footer = ""
