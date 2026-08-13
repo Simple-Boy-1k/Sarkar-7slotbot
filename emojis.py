8a502f608d1454eab702e428f9432ebdd664a3e9
𝘚𝘪𝘮𝘱𝘭𝘦_𝘉𝘰𝘺 <sksahnawaj89@gmail.com>
2026-08-13 10:25:02 +0530

    Update emojis.py

--- emojis.py
+++ emojis.py
@@ -3,13 +3,13 @@
 # (Agar ID nahi hai toh khali "" chhod dein, normal emoji chalega)
 # =========================================================
 
-ID_WELCOME_HEAD     = ""  # 🔴 Mark 1: Welcome se pehle (e.g. 🔥)
-ID_KEY_HEAD_LEFT1   = ""  # 🔴 Mark 2: How To Get Key ke left me pehla (e.g. 🟢)
-ID_KEY_HEAD_LEFT2   = ""  # 🔴 Mark 2: How To Get Key ke left me doosra (e.g. ⚡)
-ID_KEY_HEAD_RIGHT1  = ""  # 🔴 Mark 3: How To Get Key ke right me pehla (e.g. 💨)
-ID_KEY_HEAD_RIGHT2  = ""  # 🔴 Mark 3: How To Get Key ke right me doosra (e.g. 📈)
-ID_GET_KEY_LEFT     = ""  # 🔴 Mark 4: GET KEY ke left me (e.g. 🤫)
-ID_GET_KEY_RIGHT    = ""  # 🔴 Mark 5: GET KEY ke right me (e.g. 🔔)
+ID_WELCOME_HEAD     = "6077883305187876926"  # 🔴 Mark 1: Welcome se pehle (e.g. 🔥)
+ID_KEY_HEAD_LEFT1   = "6269194596094317437"  # 🔴 Mark 2: How To Get Key ke left me pehla (e.g. 🟢)
+ID_KEY_HEAD_LEFT2   = "6079974593483775722"  # 🔴 Mark 2: How To Get Key ke left me doosra (e.g. ⚡)
+ID_KEY_HEAD_RIGHT1  = "6237552274944564087"  # 🔴 Mark 3: How To Get Key ke right me pehla (e.g. 💨)
+ID_KEY_HEAD_RIGHT2  = "6244425785986257276"  # 🔴 Mark 3: How To Get Key ke right me doosra (e.g. 📈)
+ID_GET_KEY_LEFT     = "6010103971023166824"  # 🔴 Mark 4: GET KEY ke left me (e.g. 🤫)
+ID_GET_KEY_RIGHT    = "6271271702408204490"  # 🔴 Mark 5: GET KEY ke right me (e.g. 🔔)
 
 # Helper Function
 def get_emoji(emoji_id: str, fallback: str) -> str:
